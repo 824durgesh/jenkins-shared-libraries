318b694343a8d74e17fd8a1364a570e50b706950
824durgesh <70070878+824durgesh@users.noreply.github.com>
2025-07-24 12:33:54 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,9 @@
-// vars/docker_push.groovy
 def call(imageName, tag, dockerhubUser) {
-    withCredentials([usernamePassword(credentialsId: 'dockerHubCreds', usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPass')]) {
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerHubCreds',
+        usernameVariable: 'dockerHubUser',
+        passwordVariable: 'dockerHubPass'
+    )]) {
         sh "echo \$dockerHubPass | docker login -u \$dockerHubUser --password-stdin"
         sh "docker tag ${imageName}:${tag} ${dockerhubUser}/${imageName}:${tag}"
         sh "docker push ${dockerhubUser}/${imageName}:${tag}"
